ce66bd1d143ffcb0de71590c9d9b02a9d18dee82
jun2212 <hsj890509@gmail.com>
2023-02-06 03:48:23 +0900

    feat: 날짜관련 유틸함수

---
+++ src/utils/dateUtils.js
@@ -0,0 +1,48 @@
+const getCurrentDate = () => {
+  const date = new Date();
+  const currentYear = date.getFullYear();
+  const currentMonth = date.getMonth() + 1;
+  const currentDay = date.getDate();
+
+  return { currentYear, currentMonth, currentDay };
+};
+
+const getMonthStartDay = (year, month) => {
+  const monthStartDay = new Date(year, month, 1);
+
+  return monthStartDay.getDay();
+};
+
+const getMonthLastDate = (year, month) => {
+  const monthLastDate = new Date(year, month, 0);
+
+  return monthLastDate.getDate();
+};
+
+const getPreviousMonth = (year, month) => {
+  if (month === 0) {
+    return { year: year - 1, month: 11 };
+  }
+  return { year: year, month: month - 1 };
+};
+
+const getWeekCount = (monthStartDay, monthLastDate) => {
+  const WeekCount = Math.ceil((monthStartDay + monthLastDate) / 7);
+
+  return WeekCount;
+};
+
+const getPrevDate = (date) => {
+  if (date.month === 0) {
+    return { year: date.year - 1, month: 11, day: date.day };
+  }
+  return { year: date.year, month: date.month - 1, day: date.day };
+};
+
+export {
+  getCurrentDate,
+  getMonthStartDay,
+  getMonthLastDate,
+  getPreviousMonth,
+  getWeekCount,
+};
