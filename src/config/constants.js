b4df065c99d2cc91574fe10467d4ac11de3b13ea
jun2212 <hsj890509@gmail.com>
2023-02-06 03:49:50 +0900

    feat: constants 파일

---
+++ src/config/constants.js
@@ -0,0 +1,45 @@
+export const MONTH_STRING = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export const WEEK = [
+  {
+    day: "일",
+    color: "red",
+  },
+  {
+    day: "월",
+    color: "",
+  },
+  {
+    day: "화",
+    color: "",
+  },
+  {
+    day: "수",
+    color: "",
+  },
+  {
+    day: "목",
+    color: "",
+  },
+  {
+    day: "금",
+    color: "",
+  },
+  {
+    day: "토",
+    color: "blue",
+  },
+];
