ea49b04cef2e53d92a04f16ba07395da263b6933
jun2212 <hsj890509@gmail.com>
2023-02-06 03:50:58 +0900

    feat: 달력 스크린에 월, 주 컴포넌트 적용

--- src/screens/CalendarScreen/CalendarScreen.js
+++ src/screens/CalendarScreen/CalendarScreen.js
@@ -1,11 +1,37 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
+
+import { CalendarHeader } from "../../components/CalendarHeader/CalendarHeader";
+import { DayOfWeek } from "../../components/DayOfWeek/DayOfWeek";
+import { CalendarMonthContainer } from "../../components/CalendarMonthContainer/CalendarMonthContainer";
+
+import { getCurrentDate } from "../../utils/dateUtils";
+import { MONTH_STRING } from "../../config/constants";
 
 function CalendarScreen() {
+  const [targetDate, setTargetDate] = useState({});
+  const targetMonthString = MONTH_STRING[targetDate.currentMonth];
+  const targetYear = targetDate.currentYear;
+  const headerProps = {
+    targetMonthString,
+    targetYear,
+  };
+
+  useState(() => {
+    setTargetDate(getCurrentDate());
+  }, []);
+
   return (
     <View>
-      <Text>Calendar!!</Text>
+      <CalendarHeader {...headerProps} />
+      <DayOfWeek />
+      <CalendarMonthContainer {...targetDate} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  headerButton: { color: "blue" },
+});
 
 export { CalendarScreen };
